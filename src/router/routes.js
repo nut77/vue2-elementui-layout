64a20992c1ae091662ca1cc0b958efe40d21c43c
nut77 <sunnynut77@gmail.com>
2021-04-21 09:59:18 +0800

    feature-对生成路由组件方法做了补充说明

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,11 +7,12 @@ import MENU_LIST from './config';
  * path 导航对应的路径
  * redirect 非必填，重定向地址
  * componentPath 该页面对应的组件地址
+ * meta object-存一些额外的附件数据
  * meta-index number-导航层级，1表示一级导航，依次类推
  * meta-hasWrapperTop boolean-表示该页面是否具有顶部模块
  * meta-hasWrapperLeft boolean-表示该页面是否具有左侧模块
  * meta-iconClass 表示该导航模块应用的css-class,
- * meta-isNav 是否是导航
+ * meta-isNav boolean-是否是导航
  */
 const COMPONENT_EMPTY_PATH = 'WrapperEmpty.vue';
 function getMenuItemConfig(index, name, path, componentPath, config = {}, children = []) {
@@ -43,12 +44,16 @@ function getRoutes() {
   return routes;
 }
 
-// 处理异步加载页面组件，传递组件文件路径（必须是相对路径）。组件路径如果是字面量直接引用就行，参数的话就需要用这个方法。
+/**
+ * 处理异步加载页面组件，传递组件文件路径（必须是相对路径）。组件路径如果是字面量直接引用就行，参数的话就需要用这个方法。
+ * require引用文件（模块），必须是字符串（可以是字符串+变量（路径必须是相对路径）），直接引用字符串变量会报错
+ * 方法体中两种引用组件的写法都可以，核心就是require引入，import引入必须是字面量
+ */
 function loadComponent(componentPath) {
   if (componentPath === COMPONENT_EMPTY_PATH) {
     return () => Promise.resolve(require(`@l/${componentPath}`).default);
   }
-  return () => Promise.resolve(require(`@p/${componentPath}`).default);
+  return resolve => require([`@p/${componentPath}`], resolve);
 }
 
 export default getRoutes();
