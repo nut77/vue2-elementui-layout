406c6ed57c7e45f342ded7904d703c21dc515c64
nut77 <sunnynut77@gmail.com>
2021-05-19 16:41:14 +0800

    feature-优化表单验证代码

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,137 +1,137 @@
 // element ui 自定义表单验证规则
-import tool from '@/utils/tool';
-import * as ajax from '@/utils/request';
+import Tool from '@/utils/tool';
+import Api from '@/api';
 
-const Validator = {};
-
-function codeAddValidator(value, callback, type, tip) {
-  if (!value) {
-    callback(new Error(tip + '不能为空'));
-  } else if (!tool[`is${tool.upperFirstLetter(type)}`](value)) {
-    callback(new Error(tip + '格式有误'));
-  } else {
-    const res = ajax.get('test');
-    res.data ? callback(new Error(tip + '已存在')) : callback();
+/**
+ * 表单验证前期工作 先验证是否是必填
+ * @param {Object} rule - 表单项的相关属性
+ * @param {boolean} [rule.message] - 表单项输入错误后的提示语句
+ * @param {boolean} [rule.required] - 该表单项是否必填
+ * @param {boolean} [rule.isUnique] - 该表单项需要做唯一性验证
+ * @param {string} [rule.fieldType] - 表单项类型
+ * @param {string} [rule.newPassword] - 新密码，一般确认密码才需要这个参数
+ * @param {function|null} [handler = null] - 返回 除表单项是否必填验证后，其它后续验证工作过程中的错误信息
+ */
+async function validator(rule, value, callback, handler = null) {
+  value = typeof value === 'string' ? value.trim() : value;
+  const isValueEmpty = value === 0 ? false : !value;
+  // 非必填且无值 不继续验证
+  if (!rule.required && isValueEmpty) {
+    callback();
+    return false;
   }
+  // 必填且无值 提示为空，不继续验证
+  if (rule.required && isValueEmpty) {
+    const msg = rule.message ? rule.message : `请输入${rule.fieldType || ''}`;
+    debugger;
+    callback(new Error(msg));
+    return false;
+  }
+  const msg = handler ? await handler(value) : '';
+  msg ? callback(new Error(msg)) : callback();
 }
 
-// 验证是否为空
-Validator.required = (rule, value, callback) => {
-  if (!value) {
-    callback(new Error('不能为空'));
-  } else {
-    callback();
-  }
-};
+// 验证必填
+const required = (rule, value, callback) => validator(rule, value, callback);
 
 // 验证ip
-Validator.ip = (rule, value, callback) => {
-  if (!value) {
-    callback(new Error('IP不能为空'));
-  } else if (!tool.isIp(value)) {
-    callback(new Error('IP格式有误'));
-  } else {
-    callback();
-  }
-};
-Validator.ipUnique = (rule, value, callback) => {
-  codeAddValidator(value, callback, 'ip', 'IP');
-};
+const ip = (rule, value, callback) => validator(rule, value, callback, async (value) => {
+  let msg = !Tool.isIp(value) ? 'IP格式有误' : '';
+  rule.isUnique && (await Api.get('test')).data && (msg = 'IP已存在');
+  return msg;
+});
 
 // 验证域名
-Validator.domain = (rule, value, callback) => {
-  if (!value) {
-    callback(new Error('域名不能为空'));
-  } else if (!tool.isDomain(value)) {
-    callback(new Error('域名格式有误'));
-  } else {
-    callback();
-  }
-};
-Validator.domainUnique = (rule, value, callback) => {
-  codeAddValidator(value, callback, 'domain', '域名');
-};
+const domain = (rule, value, callback) => validator(rule, value, callback, async (value) => {
+  let msg = !Tool.isDomain(value) ? '域名格式有误' : '';
+  rule.isUnique && (await Api.get('test')).data && (msg = '域名已存在');
+  return msg;
+});
 
 // 验证HASH
-Validator.hash = (rule, value, callback) => {
-  if (!value) {
-    callback(new Error('HASH不能为空'));
-  } else if (!tool.isHash(value)) {
-    callback(new Error('HASH格式有误'));
-  } else {
-    callback();
-  }
-};
-Validator.hashUnique = (rule, value, callback) => {
-  codeAddValidator(value, callback, 'hash', 'HASH');
-};
+const hash = (rule, value, callback) => validator(rule, value, callback, async (value) => {
+  let msg = !Tool.isHash(value) ? 'HASH格式有误' : '';
+  rule.isUnique && (await Api.get('test')).data && (msg = 'HASH已存在');
+  return msg;
+});
 
 // 验证端口
-Validator.port = (rule, value, callback) => {
-  if (!value && value !== 0) {
-    callback(new Error('端口不能为空'));
-  } else if (!Number.isInteger(value)) {
-    callback(new Error('请输入整数值'));
-  } else if (value < 1 || value > 65535) {
-    callback(new Error('端口范围有误'));
-  } else {
-    callback();
-  }
-};
-
-// 验证手机号
-Validator.phoneUnique = (rule, value, callback) => {
-  codeAddValidator(value, callback, 'phone', '手机号');
-};
+const port = (rule, value, callback) => validator(rule, value, callback, (value) => {
+  let msg = '';
+  if (!Number.isInteger(value)) msg = '请输入整数值';
+  if (!Tool.isPort(value)) msg = '端口范围有误';
+  return msg;
+});
 
 // 验证压缩包文件格式是否正确
-Validator.file = (rule, value, callback) => {
+const file = (rule, value, callback) => validator(rule, value, callback, (value) => {
   let file;
+  let msg = '';
   const dialogElements = document.querySelectorAll('.el-dialog__wrapper');
-
   for (const ele of dialogElements) {
     if (ele.style.display !== 'none') {
       file = ele.querySelector('.el-dialog-file');
       break;
     }
   }
-  if (!value) {
-    callback(new Error('请上传文件'));
-  } else if (!/\.(zip)$/.test(value)) {
-    callback(new Error('文件格式有误'));
-  } else if (file && file.files[0].size > 500 * 1024 * 1024) {
-    callback(new Error('请上传500M以内的文件'));
-  } else {
-    callback();
-  }
-};
+  if (!/\.(zip)$/.test(value)) msg = '文件格式有误';
+  if (file && file.files[0].size > 500 * 1024 * 1024) msg = '请上传500M以内的文件';
+  return msg;
+});
 
 // 验证邮箱账号
-Validator.emailUnique = (rule, value, callback) => {
-  codeAddValidator(value, callback, 'email', '邮箱');
-};
-Validator.isEmail = (rule, value, callback) => {
-  if (!value) {
-    callback(new Error('邮箱不能为空'));
-  } else if (!tool.isEmail(value)) {
-    callback(new Error('邮箱格式有误'));
-  } else {
-    callback();
-  }
-};
-Validator.isEmailArr = (rule, value, callback) => {
-  if (!value) {
-    callback();
-    return false;
-  }
+const email = (rule, value, callback) => validator(rule, value, callback, async (value) => {
+  if (!Tool.isEmail(value)) return '邮箱格式有误';
+  if (rule.isUnique && (await Api.get('test')).data) return '邮箱已存在';
+});
+const emailArr = (rule, value, callback) => validator(rule, value, callback, (value) => {
   const arr = value.split(',');
   for (const item of arr) {
-    if (!tool.isEmail(item)) {
-      callback(new Error('邮箱格式有误'));
-      return false;
+    if (!Tool.isEmail(item)) {
+      return '邮箱格式有误';
     }
   }
-  callback();
-};
+});
 
-export default Validator;
+// 密码
+const password = (rule, value, callback) => validator(rule, value, callback, (value) => {
+  const newPassword = rule.newPassword;
+  const type = rule.fieldType;
+  const ERROR_MSG = {
+    ILLEGAL: `${type}为8~20位大小写英文字母和数字（特殊字符可选）混合`,
+    NOT_REPEAT: '确认密码和新密码不同',
+    NOT_SAME: '两次输入密码不同'
+  };
+  if (!Tool.isValidPassword(value, false)) return ERROR_MSG.ILLEGAL;
+  if (type === '确认密码' && value !== newPassword) return ERROR_MSG.NOT_REPEAT;
+  if (type === '重复密码' && value !== newPassword) return ERROR_MSG.NOT_SAME;
+});
+
+/**
+ * 验证任务-分析目标格式是否正确
+ * @param {string} [rule.targetType = ''] 本表单项依赖的上一项输入
+ */
+const taskTarget = (rule, value, callback) => validator(rule, value, callback, (value) => {
+  const type = rule.targetType;
+  let msg = '';
+  if (!type) msg = '请选择任务类型';
+  if (type === '域名' && !Tool.isDomain(value)) msg = '输入的域名格式有误';
+  if (type === 'IP' && !Tool.isIp(value)) msg = '输入的IP格式有误';
+  if (type === 'URL' && !Tool.isUrl(value)) msg = '输入的URL格式有误';
+  if (type === '样本' && !Tool.isHash(value)) msg = '输入的样本HASH格式有误';
+  if (type === '邮箱' && !Tool.isEmail(value)) msg = '输入的邮箱格式有误';
+  return msg;
+});
+
+export default {
+  required,
+  ip,
+  domain,
+  email,
+  emailArr,
+  hash,
+  port,
+  file,
+  taskTarget,
+  password
+};
