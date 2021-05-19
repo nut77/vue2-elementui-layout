b24ac0d31bc2b2b989fdfd39dc3ad35e9f4b9db1
nut77 <sunnynut77@gmail.com>
2021-05-19 17:28:25 +0800

    feature-更新公共dict.js示例

--- src/utils/dict.js
+++ src/utils/dict.js
@@ -1,38 +1,34 @@
-// 相关键键值对字典
-import * as ajax from '@/utils/request.js';
+// 相关键值对字典
+import Api from '@/api';
 
 const Dict = {};
 
-Dict.threatLevColor = [
-  'rgba(246, 90, 75, 0.2)', 'rgba(246, 90, 75, 0.3)', 'rgba(246, 90, 75, 0.4)',
-  'rgba(246, 90, 75, 0.5)', 'rgba(246, 90, 75, 0.6)', 'rgba(246, 90, 75, 0.8)', 'rgba(246, 90, 75, 1)'];
-
-Dict.level = [
-  {label: '灰名单', value: 1},
-  {label: '白名单', value: 2},
-  {label: '黑名单', value: 3}
+// 任务状态
+Dict.taskStatus = [
+  {label: '已创建', value: '已创建', status: 'primary'},
+  {label: '准备开始', value: '准备开始', status: 'info'},
+  {label: '分析中', value: '分析中', status: 'warning'},
+  {label: '分析中止', value: '分析中止', status: 'danger'},
+  {label: '分析异常', value: '分析异常', status: 'danger'},
+  {label: '分析完成', value: '分析完成', status: 'success'}
 ];
 
-// 威胁等级 （样本、邮件）
-Dict.threatLevel = [
-  {label: '正常', value: 0},
-  {label: '低危', value: 1},
-  {label: '中危', value: 2},
-  {label: '高危', value: 3},
-  {label: '危急', value: 4}
+// 任务类型
+Dict.taskType = [
+  {label: '域名', value: '域名', cls: 'fa fa-internet-explorer'},
+  {label: 'IP', value: 'IP', cls: 'fa fa-info'},
+  {label: 'URL', value: 'URL', cls: 'fa fa-link'},
+  {label: '样本', value: '样本', cls: 'fa fa-bug'},
+  {label: '邮箱', value: '邮箱', cls: 'fa fa-envelope'}
 ];
 
-Dict.state = [
-  {label: '正在检测', value: 0},
-  {label: '检测完成', value: 1},
-  {label: '检测异常', value: 2}
-];
-
-Dict.updateCountry = async function() {
+// 分析模型
+Dict.updateAnalysisModel = async function() {
   try {
-    const res = await ajax.get('test');
-    Dict.country = res.data.data;
+    const res = await Api.taskCenter.getAllModel();
+    Dict.analysisModel = res.data;
   } catch (e) {
+    Dict.analysisModel = [];
     console.log(e);
   }
 };
