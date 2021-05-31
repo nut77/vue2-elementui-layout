053a50305719a65fafb80c257badf72757a49e60
nut77 <sunnynut77@gmail.com>
2021-05-31 19:41:57 +0800

    feature-添加BaseDialog组件

--- src/components/BaseDialog.vue
+++ src/components/BaseDialog.vue
@@ -1,8 +1,61 @@
 <template>
-  <div></div>
+  <el-dialog
+    :center="true"
+    :loading="isLoading"
+    :width="width"
+    :visible.sync="show"
+    :close-on-click-modal="false"
+    :close-on-press-escape="false"
+    :append-to-body="true"
+    @closed="$emit('dialogClose')"
+    custom-class="base-dialog-container">
+    <template slot="title">
+      <base-text :content="title" class="el-dialog__title"></base-text>
+    </template>
+    <slot></slot>
+    <template slot="footer">
+      <slot name="dialogFooter"></slot>
+    </template>
+  </el-dialog>
 </template>
 
 <script>
 export default {
+  name: 'BaseDialog',
+  props: {
+    width: {
+      type: String,
+      default: () => {
+        return document.body.clientWidth <= 1440 ? '40%' : '30%';
+      }
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    nodeId: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      show: false,
+      isLoading: false
+    };
+  },
+  methods: {
+    loadingOpen() {
+      this.isLoading = true;
+    },
+    loadingClose() {
+      this.isLoading = false;
+    }
+  },
+  watch: {
+    nodeId: function (val) {
+      this.show = !!val;
+    }
+  }
 };
 </script>
