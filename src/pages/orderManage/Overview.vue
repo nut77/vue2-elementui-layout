21acef3e3068ae134c821680d601c605e68847af
nut77 <sunnynut77@gmail.com>
2021-06-14 22:33:09 +0800

    feature-订单管理-订单总栏部分（后续添加echarts配置）

--- src/pages/orderManage/Overview.vue
+++ src/pages/orderManage/Overview.vue
@@ -1,13 +1,58 @@
 <template>
-  <div>订单管理-Overview</div>
+  <div class="dflex box">
+    <div class="box-top dflex">
+      <div class="box-top-item" v-for="item of boxTopList" :key="item.title">
+        <p class="box-title">{{item.title}}</p>
+        <div class="box-content" :class="item.data ? '' : 'box--empty'"></div>
+      </div>
+    </div>
+    <div class="box-bottom mgt20 flex">
+      <p class="box-title">订单趋势</p>
+      <div class="box-content box--empty"></div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Overview'
+  name: 'Overview',
+  data() {
+    return {
+      boxTopList: [
+        {title: '已完成订单', data: null, chart: null},
+        {title: '已完成订单分类统计', data: null, chart: null},
+        {title: '未完成订单', data: null, chart: null},
+        {title: '未完成订单分类统计', data: null, chart: null}
+      ]
+    };
+  }
 };
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+  .box {
+    flex-direction: column;
+  }
+  .box-top {
+    justify-content: space-between;
+    height: 250px;
+    min-height: 250px;
+  }
+  .box-top-item {
+    min-width: 250px;
+  }
+  .box-top-item,
+  .box-bottom {
+    .dflex;
+    flex-direction: column;
+    .pd15;
+    background: @background-color-lighter;
+    border-radius: 5px;
+  }
+  .box-bottom {
+    min-height: 300px;
+  }
+  .box-content {
+    .flex;
+  }
 </style>
