3a4ca15ef471f18e57d4355c500f95e0bf7e52d9
nut77 <sunnynut77@gmail.com>
2021-06-28 17:27:05 +0800

    feature-修改favicon、修改首页单词拼写错误

--- src/pages/home/Index.vue
+++ src/pages/home/Index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    首页
     <div class="pd10 dflex" v-for="(item, index) of datePickerList" :key="index">
       <span class="label">{{item.label}}</span>
       <base-date-picker
@@ -8,7 +7,7 @@
         :isRange="item.isRange"
         :datePickerValueType="item.datePickerValueType"
         @change="value => item.datePickerValue = value"/>
-      <span class="value">{{/string/i.test(item.datePickerValueType) ? '字符串' : '时间戳'}}：{{item.datePickerValue}}</span>
+      <span class="value">值为{{/string/i.test(item.datePickerValueType) ? '字符串' : '时间戳'}}：{{item.datePickerValue}}</span>
     </div>
   </div>
 </template>
@@ -19,16 +18,16 @@ export default {
   data() {
     return {
       datePickerList: [
-        {lable: '选择日期', type: 'date', isRange: false, datePickerValueType: 'string', datePickerValue: ''},
-        {lable: '选择日期', type: 'date', isRange: false, datePickerValueType: 'integer', datePickerValue: ''},
-        {lable: '选择日期范围', type: 'date', isRange: true, datePickerValueType: 'string', datePickerValue: ''},
-        {lable: '选择日期范围', type: 'date', isRange: true, datePickerValueType: 'arrayString', datePickerValue: ''},
-        {lable: '选择日期范围', type: 'date', isRange: true, datePickerValueType: 'arrayInteger', datePickerValue: ''},
-        {lable: '选择时间', isRange: false, datePickerValueType: 'string', datePickerValue: ''},
-        {lable: '选择时间', isRange: false, datePickerValueType: 'integer', datePickerValue: ''},
-        {lable: '选择时间范围', isRange: true, datePickerValueType: 'string', datePickerValue: ''},
-        {lable: '选择时间范围', isRange: true, datePickerValueType: 'arrayString', datePickerValue: ''},
-        {lable: '选择时间范围', isRange: true, datePickerValueType: 'arrayInteger', datePickerValue: ''}
+        {label: '选择日期', type: 'date', isRange: false, datePickerValueType: 'string', datePickerValue: ''},
+        {label: '选择日期', type: 'date', isRange: false, datePickerValueType: 'integer', datePickerValue: ''},
+        {label: '选择日期范围', type: 'date', isRange: true, datePickerValueType: 'string', datePickerValue: ''},
+        {label: '选择日期范围', type: 'date', isRange: true, datePickerValueType: 'arrayString', datePickerValue: ''},
+        {label: '选择日期范围', type: 'date', isRange: true, datePickerValueType: 'arrayInteger', datePickerValue: ''},
+        {label: '选择时间', isRange: false, datePickerValueType: 'string', datePickerValue: ''},
+        {label: '选择时间', isRange: false, datePickerValueType: 'integer', datePickerValue: ''},
+        {label: '选择时间范围', isRange: true, datePickerValueType: 'string', datePickerValue: ''},
+        {label: '选择时间范围', isRange: true, datePickerValueType: 'arrayString', datePickerValue: ''},
+        {label: '选择时间范围', isRange: true, datePickerValueType: 'arrayInteger', datePickerValue: ''}
       ]
     };
   }
@@ -41,10 +40,11 @@ export default {
   }
   .label {
     width: 100px;
-    .mgr10;
+    .mgr15;
     .tr;
   }
   .value {
+    font-size: 12px;
     .mgl10;
   }
 </style>
